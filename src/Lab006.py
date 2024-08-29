86a6a2f7bd3e1180738bc52a47d61293dd1dd041
win <spandashrihd@gmail.com>
2024-08-29 18:18:18 +0530

    user input exercise

---
+++ src/Lab006.py
@@ -0,0 +1,20 @@
+#Take a user input a,b and do sum, mul,div and sub
+
+a=int(input("enter a first number: "))
+b=int(input("enter a second number: "))
+# sum1=a+b
+# mul=a*b
+# div=a/b
+# sub=a-b
+#
+# print("sum is: ",sum1)
+# print("mul is: ", mul)
+# print("div is ", div)
+# print("sub is: ", sub)
+
+print("sum is: ",a+b)
+print("mul is: ", a*b)
+print("div is ", a/b)
+print("sub is: ", a-b)
+
+
