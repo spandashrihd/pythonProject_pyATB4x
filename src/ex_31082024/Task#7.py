092a00a35ff21accb4c6f6ffad94a4dc368d703e
win <spandashrihd@gmail.com>
2024-09-01 14:04:04 +0530

    Leap Year Task#7

---
+++ src/ex_31082024/Task#7.py
@@ -0,0 +1,6 @@
+#check if a given year is leap year or not
+year= int(input("enter a year: "))
+if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
